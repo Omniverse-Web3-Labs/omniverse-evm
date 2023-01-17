26637be144c08a1015840e1fa55ed5c0a598f16e
xiyu1984 <shanellyy@126.com>
2023-01-17 12:28:06 +0800

    update

--- contracts/contracts/OmniverseTransactionData.sol
+++ contracts/contracts/OmniverseTransactionData.sol
@@ -3,20 +3,23 @@ pragma solidity >=0.8.0 <0.9.0;
 
 /**
  * @dev Omniverse transaction data structure, different `op` indicates different type of omniverse transaction
- * nonce: The number of o-transactions sent from a user
- * chainId: The chain where the o-transaction is initiated
- * initiator: The contract address from which the o-transaction is initiated
- * from: The Omniverse account which signs the o-transaction
- * op: The operation type
- * data: The operation data
- * amount: The amount of token which is operated
- * signature: The signature of the above informations
+ * @Member nonce: The serial number of an o-transactions sent from an Omniverse Account. If the current nonce of an o-account is k, the valid nonce in the next o-transaction is k+1. 
+ * @Member chainId: The chain where the o-transaction is initiated
+ * @Member initiator: The contract address from which the o-transaction is initiated
+ * @Member from: The Omniverse account which signs the o-transaction
+ * @Member op: The operation type. NOTE op: 0-31 are reserved values, 32-255 are custom values
+ *             op: 0 Transfers omniverse token `amount` from user `from` to user `data`, `from` MUST have at least `amount` token
+ *             op: 1 User `from` mints token `amount` to user `data`
+ *             op: 2 User `from` burns token `amount` from user `data`
+ * @Member data: The operation data. This sector could be empty and is determined by `op`
+ * @Member amount: The amount of token which is operated
  * 
- * NOTE op: 0-31 are reserved values, 32-255 are custom values
+ * @Member signature: The signature of the above informations. 
+ *                    Firstly, the above sectors are combined as 
+ *                    `bytes memory rawData = abi.encodePacked(uint128(_data.nonce), _data.chainId, _data.initiator, _data.from, _data.op, _data.data, uint128(_data.amount));`
+ *                    The it is hashed by `keccak256(rawData)`
+ *                    The signature is to the hashed value.
  * 
- * op: 0 Transfers omniverse token `amount` from user `from` to user `data`, `from` MUST have at least `amount` token
- * op: 1 User `from` mints token `amount` to user `data`
- * op: 2 User `from` burns token `amount` from user `data`
  */
 struct OmniverseTransactionData {
     uint256 nonce;
--- contracts/contracts/OmniverseTransactionDataNFT.sol
+++ contracts/contracts/OmniverseTransactionDataNFT.sol
@@ -3,22 +3,24 @@ pragma solidity >=0.8.0 <0.9.0;
 
 /**
  * @dev Omniverse transaction data structure, different `op` indicates different type of omniverse transaction
- * nonce: The number of o-transactions sent from a user
- * chainId: The chain where the o-transaction is initiated
- * initiator: The contract address from which the o-transaction is initiated
- * from: The Omniverse account which signs the o-transaction
- * op: The operation type
- * data: The operation data
- * tokenId: the token id
- * signature: The signature of the above informations
+ * @Member nonce: The serial number of an o-transactions sent from an Omniverse Account. If the current nonce of an o-account is k, the valid nonce in the next o-transaction is k+1. 
+ * @Member chainId: The chain where the o-transaction is initiated
+ * @Member initiator: The contract address from which the o-transaction is initiated
+ * @Member from: The Omniverse account which signs the o-transaction
+ * @Member op: The operation type. NOTE op: 0-31 are reserved values, 32-255 are custom values
+ *             op: 0 Transfers omniverse token `tokenId` from user `from` to user `data`, the token `tokenId` MUST be owned by `from`
+ *             op: 1 User `from` mints token `tokenId` to user `data`
+ *             op: 2 User `from` burns token `tokenId` from user `data`
+ * @Member data: The operation data. This sector could be empty and is determined by `op`
+ * @Member tokenId: the token id
  * 
- * NOTE op: 0-31 are reserved values, 32-255 are custom values
- * 
- * op: 0 Transfers omniverse token `tokenId` from user `from` to user `data`, the token `tokenId` MUST be owned by `from`
- * op: 1 User `from` mints token `tokenId` to user `data`
- * op: 2 User `from` burns token `tokenId` from user `data`
+ * @Member signature: The signature of the above informations
+ *                    Firstly, the above sectors are combined as 
+ *                    `bytes memory rawData = abi.encodePacked(uint128(_data.nonce), _data.chainId, _data.initiator, _data.from, _data.op, _data.data, uint128(_data.tokenId));`
+ *                    The it is hashed by `keccak256(rawData)`
+ *                    The signature is to the hashed value.
  */
-struct OmniverseTransactionData {
+struct OmniverseTransactionDataNFT {
     uint256 nonce;
     uint32 chainId;
     bytes initiator;
