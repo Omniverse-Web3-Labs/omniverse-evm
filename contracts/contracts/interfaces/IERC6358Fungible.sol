dfcb7ceb58e139f5c49fabca2dd6b901bdea3cf1
xiyu1984 <shanellyy@126.com>
2023-01-20 09:49:08 +0800

    v0.1.0

---
+++ contracts/contracts/interfaces/IERC6358Fungible.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./IERC6358.sol";
+
+/**
+ * @notice Interface of the omniverse fungible token, which inherits {IERC6358}
+ */
+interface IERC6358Fungible is IERC6358 {
+    /**
+     * @notice Get the omniverse balance of a user `_pk`
+     * @param _pk Omniverse account to be queried
+     * @return Returns the omniverse balance of a user `_pk`
+     */
+    function omniverseBalanceOf(bytes calldata _pk) external view returns (uint256);
+}
